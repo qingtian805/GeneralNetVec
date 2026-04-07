b30a34c094aafe686723fd09cb6257734df6d5f0
Kevin Li <qingtian805@qq.com>
2026-04-07 19:59:27 +0800

    OnlineExam now with no lock issue

--- exam_online.py
+++ exam_online.py
@@ -1,10 +1,16 @@
-from queue import Queue, Empty
+from queue import Queue
 from threading import Thread
+from struct import pack
 import pickle as pkl
-from scapy.sendrecv import sniff
+from scapy.sendrecv import AsyncSniffer
 
 from exam import Exam
 from net_vec.examinator import OLExaminator
+
+ABORT_MSG = b"ABO"
+ACK_MSG   = b"ACK"
+END_MSG   = b"END"
+START_MSG = b"STA"
 
 class OnlineExam(Exam):
     def __init__(
@@ -13,6 +19,12 @@ class OnlineExam(Exam):
             batch_size: int = 100,
             buffer_size: int = 0
             ):
+        """
+        :param ol_examinator: 一个在线评估器，要求能够评价一个 scapy.plist.PacketList
+        类型数据
+        :param batch_size: 一次送入评估器的流量数量
+        :param buffer_size: 缓冲区大小，如果 <= 0，则不限制缓冲区大小
+        """
         
         getattr(ol_examinator, "exam_pkt")
         
@@ -21,11 +33,11 @@ class OnlineExam(Exam):
 
         self.buffer = Queue(buffer_size)
         self.rmse_list = []
+        self.sniffer = None # type: AsyncSniffer
 
     def dump_result(self, dump_file_prefix):
         with open(f"{dump_file_prefix}.pkt", "wb") as f:
-            pkl.dump(self.origin_rmse, f)
-            pkl.dump(self.manipu_rmse, f)
+            pkl.dump(self.rmse_list, f)
 
     def capture(self, packet):
         if not self.buffer.full():
@@ -35,26 +47,79 @@ class OnlineExam(Exam):
     
     def process(self):
         self.examinator.prepare_exam()
-        batch = []
+        self.batch = []
         while True:
             print("buff: {}".format(self.buffer.qsize()))
             
-            batch.append(self.buffer.get())
+            pkt = self.buffer.get()
+            if pkt is None:
+                break
+            self.batch.append(pkt)
 
-            if len(batch) >= self.batch_size:
-                rmse = self.examinator.exam_pkt(batch)
-                batch.clear()
+            if len(self.batch) >= self.batch_size:
+                rmse = self.examinator.exam_pkt(self.batch)
+                self.batch.clear()
                 self.rmse_list += rmse
+        
+    def flush(self):
+        """
+        将最后的数据全部取出并完成评估，
+        """
+        if len(self.batch) != 0:
+            self.rmse_list += self.examinator.exam_pkt(self.batch)
 
     def exam(self, interface = None ,filter = ""):
-        self.running = True
+        """
+        :param interface: sniff on which interface, defaults to scapy.conf.iface
+        :param filter: Berkeley Packet Filter, see [This site](https://www.ibm.com/docs/en/qsip/7.5.0?topic=queries-berkeley-packet-filters)
+        """
         p_thread = Thread(target=self.process)
-        p_thread.daemon = True
         p_thread.start()
         
-        sniff(prn=self.capture, store=False, filter=filter, iface=interface)
+        self.sniffer = AsyncSniffer(prn=self.capture, store=False, filter=filter, iface=interface)
+        self.sniffer.start()
 
-        print(self.rmse_list)
+    def stop(self):
+        self.sniffer.stop(join=True)
+        self.buffer.put(None)
+        self.flush()
+    
+class ExamMetricCalculator(Exam):
+    def __init__(self):
+        pass
 
-# class MetricCalulator(Exam):
+    @staticmethod
+    def cal_metric(origin_rmse, manipu_rmse):
+        """
+        :param origin_rmse: 可以是路径(str)或数据(np.array)
+        :param manipu_rmse: 可以是路径(str)或数据(np.array)
+        """
 
+        if isinstance(origin_rmse, str):
+            with open(origin_rmse) as f:
+                origin_rmse = pkl.load(f)
+
+        if isinstance(manipu_rmse, str):
+            with open(manipu_rmse) as f:
+                manipu_rmse = pkl.load(f)
+
+        calculator = ExamMetricCalculator()
+        calculator.origin_rmse = origin_rmse
+        calculator.manipu_rmse = manipu_rmse
+
+        der = calculator._detection_evasion_rate()
+        mer = calculator._malicious_evasion_rate()
+        pdr = calculator._probability_decline_rate()
+
+        return der,mer,pdr
+    
+if __name__ == "__main__":
+    from examinators import OLKitsuneExam
+    from time import sleep
+    exam = OLKitsuneExam("./exam_res/kitsune/model.pkl")
+    e = OnlineExam(exam, batch_size=20, buffer_size=10)
+
+    e.exam()
+    sleep(2)
+    e.stop()
+    print(len(e.rmse_list))
