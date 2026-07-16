794219a8838166173adc740be50504c2d8efefbc
Kevin Li <qingtian805@qq.com>
2026-07-16 16:54:41 +0800

    restore config init method

--- net_vec/config.py
+++ net_vec/config.py
@@ -90,47 +90,31 @@ class Conf:
         if pkt_list is not None:
             self.set_pkt_list(pkt_list, last_end_time)
 
-    def __init__(
-            self,
-            max_cft_pkt: int = 1,
-            max_cft_pkt_prob: float = 0.01,
-            max_time_extend: float = 6.,
-            min_time_extend: float = 3.,
-            fence_time_divider: int = 10000,
-            cft_time_divider: int = 1000,
-            proto_min_lmt: float = 1.,
-            data_max_lmt: list = [np.nan, 1500., 1480., 1460.],
-            data_min_lmt: float = 0.,
-            pkt_list: list[Packet] = None,
-            last_end_time: EDecimal = None,
-        ):
+    def __init__(self):
         """向量设置类，负责完成 nev_vec 相关基础数据结构的设置（主要是完成对于 流量-向量 可逆映射的设置）
 
         """
-        if pkt_list is not None:
-            self.set_pkt_list(pkt_list, last_end_time)
-        else:
-            self.pkt_list: list[Packet] | None  = pkt_list
-            """ 被处理的恶意包列表，用于构建向量与重构流量   \n 默认值：None """
-            self.last_end_time: EDecimal | None = last_end_time
-            """ 上一条流结束的时间   \n 默认值：None"""
-        self.max_cft_pkt: int          = max_cft_pkt
+        self.pkt_list: list[Packet] | None  = None
+        """ 被处理的恶意包列表，用于构建向量与重构流量   \n 默认值：None """
+        self.last_end_time: EDecimal | None = None
+        """ 上一条流结束的时间   \n 默认值：None"""
+        self.max_cft_pkt: int          = 1
         """ 每个原始包对应构建包的最大数量(l_c)   \n 默认值：1 """
-        self.max_cft_pkt_prob: float   = max_cft_pkt_prob
+        self.max_cft_pkt_prob: float   = 0.01
         """ 在 0-1 之间的概率,限制一个slot填入构造包的最大概率,相当于 max_cft_pkt 的最大乘数   \n 默认值：0.01 """
-        self.max_time_extend: float    = max_time_extend
+        self.max_time_extend: float    = 6.
         """ 新流量相较于原始的时间倍数(l_t)   \n 默认值 6.0 """
-        self.min_time_extend: float    = min_time_extend
+        self.min_time_extend: float    = 3.
         """ 新流量相较于原始的时间倍数(l_t)   \n 默认值 3.0 """
-        self.fence_time_divider: int   = fence_time_divider
+        self.fence_time_divider: int   = 10000
         """ 用于计算原始包之间的最小时间间隔,代表最大允许时间间隔中存在多少个可用时间位置   \n 默认值：10000 """
-        self.cft_time_divider: int     = cft_time_divider
+        self.cft_time_divider: int     = 1000
         """ 用于计算构建包与前个包之间的最小时间间隔,代表在构建包与前个包时间间隔中存在多少个可用时间位置   \n 默认值 1000 """
-        self.proto_min_lmt: float      = proto_min_lmt
+        self.proto_min_lmt: float      = 1.0
         """ 用于限制构建包最小使用的协议层数   \n 默认值：1.0"""
-        self.data_max_lmt: list[float] = data_max_lmt
+        self.data_max_lmt: list[float] = [np.nan, 1500., 1480., 1460.]
         """ 用于限制构建包的 mtu, list 的序号对应协议层数, 如序号 2 限制 2 层包的 mtu    \n 默认值：[np.nan, 1500., 1480., 1460.]"""
-        self.data_min_lmt: float       = data_min_lmt
+        self.data_min_lmt: float       = 0.
         """ 用于限制构建包最小的 mtu, 限制构建包, 无论协议层数   \n 默认值 0.0"""
 
         self._pkt_num: int | None               = None
@@ -138,5 +122,5 @@ class Conf:
         self._proto_max_lmt: list[float] | None = None
         """ [运行时参数] 用于限制构建包最大使用的协议层数 """
 
-cfg: Conf | None = None
+cfg: Conf = Conf()
 """ 设置指针，保证设置实例的单例在算法多个模块之间共享的参数设置 """
